728f1f0c98759b84f35e19791486acefde5e91cd
ziaha <ziaha@Zainab-PC.home>
2020-04-15 22:59:14 -0400

    Second Commit

--- TestProject/src/main/java/com/runner/NewToursRunner.java
+++ TestProject/src/main/java/com/runner/NewToursRunner.java
@@ -5,9 +5,10 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
 // cucumber goes by order so find finder runs first then new tours you have to make sure.
+//features = {"./src/main/resources/NewToursDemoLogin.feature",
+//"./src/main/resources/flightFinder.feature"},
 @CucumberOptions(
-		features = {"./src/main/resources/NewToursDemoLogin.feature"},
-	//			"/TestProject/src/main/resources/flightFinder.feature"},
+		features = {"./src/main/resources/NewToursDemoLogin.feature"},				
 		glue = {"com.stepdef"},
 		plugin = {"pretty", "html:target" , "json:target/cucumber.json" , 
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, 
